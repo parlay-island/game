e4e19cd49dc2d9969efe2591c804be4d9eb4c43c
Jessica Su <jessica.su@duke.edu>
2020-09-23 20:30:35 -0400

    Revert "modifying git ignore"
    This reverts commit 110bb99f9c164cf398a021fdc5368cd79e3ccf6f.

---
+++ Parlay/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject gameOverImage;
+    public Text gameOverText;
+	public Timer timerManager;
+    public float gameTime;
+
+	public static GameManager instance = null;
+    
+    void Awake()
+    {
+    	// make sure GameManager is a singleton
+    	if (instance==null) {
+    		instance = this; 
+    	} else if (instance != this) {
+    		Destroy(gameObject);
+    	}
+
+    	DontDestroyOnLoad(gameObject);
+    	initGame();
+    }
+
+    private void initGame() {
+        gameOverImage.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+        timerManager.initTimer(gameTime);
+    }
+
+    private void gameOver() {
+        gameOverImage.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        timerManager.hideTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerManager.isTimeUp()) {
+            gameOver();
+            return;
+        } else {
+            timerManager.updateTime();
+        }
+    }
+}
---
+++ Parlay/Assets/Tests/PlayTests/TimerManagerTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TimerManagerTest
+    {
+        private GameObject testObject;
+        private Timer timer;
+
+        [SetUp]
+        public void Setup() {
+            testObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TimeManager"));
+            timer = testObject.GetComponent<Timer>();
+            timer.initTimer(2f);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            GameObject.Destroy(testObject);
+        }
+
+        [UnityTest]
+        public IEnumerator ShowsTheSliderAndText()
+        {
+            Assert.IsTrue(timer.timerSlider.enabled);
+            Assert.IsTrue(timer.timerText.gameObject.activeSelf);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TimerDecreasesTimeProperly()
+        {
+            Assert.IsFalse(timer.isTimeUp());
+
+            yield return new WaitForSeconds(1f);
+            timer.updateTime();
+            
+            // time should decrease by one second
+            Assert.AreEqual(timer.timerText.text, "0:01");
+            Assert.IsFalse(timer.isTimeUp());
+        }
+
+        [UnityTest]
+        public IEnumerator TimerDoesNotGoToZero()
+        {
+            // making the timer go for more than the max (2 seconds)
+            for (int i = 0; i < 3; i++) {
+                timer.updateTime();
+                yield return new WaitForSeconds(1f);
+            }
+            
+            // Assert.AreEqual(Mathf.FloorToInt(timer.getCurrTime()), 0);
+            Assert.IsTrue(timer.isTimeUp());
+            Assert.AreEqual(timer.timerText.text, "0:00");
+        }
+    }
+}
