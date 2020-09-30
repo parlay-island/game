48c203076f875831397eef4ef7bb81822178a8e3
Holly Ansel <holly.ansel@duke.edu>
2020-09-29 20:48:32 -0400

    adding enemy prefab with animations, rigidbody2d, and boxcollider2d

---
+++ Parlay/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Range(-30f, -1f)][SerializeField] private float m_TimeReduction = -2f;
+  	[SerializeField] private Timer m_Timer;
+
+    void Update () {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      if(collision.gameObject.tag == "Player")
+      {
+        hitPlayer();
+      }
+    }
+
+    private void hitPlayer()
+    {
+      m_Timer.AddTime(m_TimeReduction);
+      Debug.Log(m_Timer.getCurrTime());
+    }
+
+}
