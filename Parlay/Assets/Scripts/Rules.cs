b94aa4762890b8a83fe7ae9f8eae77625ae91eb9
Jessica Su <js803@duke.edu>
2020-11-10 01:48:00 -0800

    added rules screens

---
+++ Parlay/Assets/Scripts/Rules.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rules : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void backToModeSelection()
+    {
+        StartCoroutine(LoadModeScreen());
+    }
+
+    IEnumerator LoadModeScreen()
+    {
+        string mode_scene_name = "ModeSelection";
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(mode_scene_name, LoadSceneMode.Single);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void goToRulesScreen(string rules_screen_name)
+    {
+        StartCoroutine(LoadRulesScreen(rules_screen_name));
+    }
+
+    IEnumerator LoadRulesScreen(string rules_screen_name)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(rules_screen_name, LoadSceneMode.Single);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Parlay/Assets/Tests/PlayTests/RulesTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class RulesTest
+    {
+        private GameObject testObject;
+        private Rules rules;
+
+
+        [SetUp]
+        public void Setup() {
+            testObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Rules/RulesManager"));
+            rules = testObject.GetComponent<Rules>();
+        }
+        
+        [TearDown]
+        public void Teardown()
+        {
+            GameObject.Destroy(testObject);
+        }
+        
+        [UnityTest, Order(1)]
+        public IEnumerator GoesToNextRulesScreen()
+        {
+            rules.goToRulesScreen("RulesScreen2");
+            yield return new WaitForSeconds(1f);
+            Scene ruleScene2 = SceneManager.GetActiveScene();
+            Assert.AreEqual("RulesScreen2", ruleScene2.name);
+        }
+        
+        [UnityTest, Order(2)]
+        public IEnumerator GoesToFirstRulesScreen()
+        {
+            rules.goToRulesScreen("RulesScreen");
+            yield return new WaitForSeconds(1f);
+            Scene ruleScene2 = SceneManager.GetActiveScene();
+            Assert.AreEqual("RulesScreen", ruleScene2.name);
+        }
+        
+        [UnityTest, Order(3)]
+        public IEnumerator GoesBackToModeScreen()
+        {
+            rules.backToModeSelection();
+            yield return new WaitForSeconds(1f);
+            Scene modeScene = SceneManager.GetActiveScene();
+            Assert.AreEqual("ModeSelection", modeScene.name);
+        }
+
+        
+    }
+}
